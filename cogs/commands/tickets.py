9950775c451d5f6e49b98d9429b64a47a53d35bb
cleeryy <clery.arqueferradou@gmail.com>
2025-01-08 15:25:33 +0100

    [+] Added and cleaned ticket's commands and events

---
+++ cogs/commands/tickets.py
@@ -0,0 +1,122 @@
+import nextcord as nc
+from nextcord.ext import commands
+from nextcord.ui import Button, View
+from time import sleep
+
+
+class TicketUtils:
+    @staticmethod
+    def user_has_permission(interaction: nc.Interaction) -> bool:
+        return (
+                interaction.user.top_role.permissions.manage_channels or
+                interaction.user.top_role.permissions.administrator or
+                interaction.user.id == interaction.guild.owner_id
+                )
+
+    @staticmethod
+    async def handle_existing_ticket_system(interaction: nc.Interaction, ticket_category: nc.CategoryChannel):
+        view = View()
+        view.add_item(Button(
+            style=nc.ButtonStyle.danger,
+            label="Supprimer l'ancien système de tickets",
+            custom_id="delete_ticket"
+            ))
+        view.add_item(Button(
+            style=nc.ButtonStyle.primary,
+            label="Annuler",
+            custom_id="cancel"
+            ))
+
+        view.interaction_check = lambda i: TicketUtils.allow_ticket_creation(i, ticket_category)
+
+        await interaction.send(
+                "Il y a déjà un système de tickets, tu veux vraiment le supprimer pour en créer un nouveau ? <:OHHHH:1287740128806309930>",
+                view=view
+                )
+
+    @staticmethod
+    async def setup_new_ticket_system(interaction: nc.Interaction, ticket_category: nc.CategoryChannel):
+        set_value("TICKET_CHANNEL_ID", interaction.channel.id)
+        set_value("TICKET_CATEGORY_ID", ticket_category.id)
+
+        await interaction.send(
+                "Le système de tickets a été superbement configuré ! <:yay:1274376322847739935>"
+                )
+
+        await TicketUtils.send_ticket_embed(interaction.channel)
+
+        sleep(3)
+        await interaction.delete_original_message()
+
+    @staticmethod
+    async def allow_ticket_creation(interaction: nc.Interaction, ticket_category: nc.CategoryChannel):
+        if interaction.data["custom_id"] == "delete_ticket":
+            set_value("TICKET_CHANNEL_ID", interaction.channel.id)
+            set_value("TICKET_CATEGORY_ID", ticket_category.id)
+
+            await interaction.message.edit(
+                    "Le système de tickets a été superbement configuré ! <:yay:1274376322847739935>",
+                    view=None
+                    )
+
+            await TicketUtils.send_ticket_embed(interaction.channel)
+
+            sleep(3)
+            await interaction.message.delete()
+        else:
+            await interaction.message.edit(
+                    "On fera la configuration plus tard. <:miamchoco:1216663553722023966>",
+                    view=None
+                    )
+
+            sleep(3)
+            await interaction.message.delete()
+
+    @staticmethod
+    async def send_ticket_embed(channel: nc.TextChannel):
+        view = View()
+        view.add_item(Button(
+            style=nc.ButtonStyle.primary,
+            label="Créer un ticket",
+            custom_id="create_ticket"
+            ))
+
+        embed = nc.Embed(
+                title="Ticket d'entrée <:petitefrog:1216663533928845322>",
+                description="Pour rentrer sur le serveur, merci d'ouvrir un ticket et répondre aux questions ! <:yay:1274376322847739935>",
+                color=nc.Color.green()
+                )
+
+        await channel.send(embed=embed, view=view)
+
+
+class ManageTicketCommand(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @nc.slash_command(description="Mettre en place le système de tickets.")
+    async def configurer_tickets(
+            self,
+            interaction: nc.Interaction,
+            ticket_category: nc.CategoryChannel = nc.SlashOption(
+                description="La catégorie où les tickets seront créés.",
+                required=True
+                )
+            ):
+        await interaction.guild.fetch_roles()
+
+        if not TicketUtils.user_has_permission(interaction):
+            await interaction.send(
+                    "Eh oh, tu tentes de faire quoi ? Pas touche à cette commande ! <:attaque:1216663550282694717>"
+                    )
+            return
+
+        if get_value("TICKET_CHANNEL_ID"):
+            await TicketUtils.handle_existing_ticket_system(interaction, ticket_category)
+        else:
+            await TicketUtils.setup_new_ticket_system(interaction, ticket_category)
+
+
+def setup(bot):
+    bot.add_cog(ManageTicketCommand(bot))
+
---
+++ cogs/events/tickets.py
@@ -0,0 +1,73 @@
+import nextcord as nc
+from nextcord.ext import commands
+
+
+class TicketEventUtils:
+    @staticmethod
+    def generate_ticket_id(user_id: int) -> int:
+        return sum([int(i) for i in str(user_id)])
+
+    @staticmethod
+    async def create_ticket_channel(
+            interaction: nc.Interaction,
+            category: nc.CategoryChannel,
+            ticket_id: int
+            ) -> nc.TextChannel:
+        return await category.create_text_channel(
+                name=f"ticket-{ticket_id}",
+                topic=str(interaction.user.id),
+                overwrites={
+                    interaction.guild.default_role: nc.PermissionOverwrite(read_messages=False),
+                    interaction.guild.get_role(get_value("STAFF_ROLE_ID")): nc.PermissionOverwrite(read_messages=True),
+                    interaction.user: nc.PermissionOverwrite(read_messages=True)
+                    }
+                )
+
+    @staticmethod
+    def create_welcome_embed() -> nc.Embed:
+        return nc.Embed(
+                title="Bienvenue à 𝐿'𝛼𝜋𝜏𝜄𝑞𝜇𝜀 ! ☕",
+                description=(
+                    "Merci de répondre aux questions suivantes afin de valider ton arrivée sur le serveur! "
+                    "Un membre du staff te répondra dès que possible. 🐸\n\n"
+                    "🍉 Quel âge as-tu ?\n"
+                    "🍉 Que cherches-tu sur ce serveur?"
+                    ),
+                color=nc.Color.green()
+                )
+
+    @staticmethod
+    async def send_and_delete_mention(channel: nc.TextChannel, user_id: int, staff_role_id: int):
+        msg = await channel.send(f"<@{user_id}><@&{staff_role_id}>")
+        await msg.delete()
+
+
+class ManageTicketEvent(commands.Cog):
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_interaction(self, interaction: nc.Interaction):
+        if interaction.type == nc.InteractionType.component:
+            if interaction.data["custom_id"] == "create_ticket":
+                await self.handle_ticket_creation(interaction)
+
+    async def handle_ticket_creation(self, interaction: nc.Interaction):
+        category: nc.CategoryChannel = await interaction.guild.fetch_channel(get_value("TICKET_CATEGORY_ID"))
+        ticket_id = TicketEventUtils.generate_ticket_id(interaction.user.id)
+
+        channel = await TicketEventUtils.create_ticket_channel(interaction, category, ticket_id)
+        embed = TicketEventUtils.create_welcome_embed()
+
+        await interaction.send("Le ticket a été créé ! <:yay:1274376322847739935>", ephemeral=True)
+        await channel.send(embed=embed)
+        await TicketEventUtils.send_and_delete_mention(
+                channel,
+                interaction.user.id,
+                get_value('STAFF_ROLE_ID')
+                )
+
+
+def setup(bot):
+    bot.add_cog(ManageTicketEvent(bot))
+
